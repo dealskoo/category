8a0a9564016b054b65c7da6d35386c7e0985620b
jabber <2898117012@qq.com>
2022-02-14 15:09:51 +0800

    fix: test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,16 +6,4 @@ use Dealskoo\Category\Providers\CategoryServiceProvider;
 
 abstract class TestCase extends \Dealskoo\Admin\Tests\TestCase
 {
-    protected function getPackageProviders($app)
-    {
-        return [
-            CategoryServiceProvider::class
-        ];
-    }
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-    }
 }
