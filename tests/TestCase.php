6b0333ca76344da9ded70829d3acceb3b261f3ce
jabber <2898117012@qq.com>
2022-02-15 18:28:47 +0800

    fix: test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,19 @@
 
 namespace Dealskoo\Category\Tests;
 
+use Dealskoo\Category\Providers\CategoryServiceProvider;
+
 abstract class TestCase extends \Dealskoo\Admin\Tests\TestCase
 {
+    protected function getPackageProviders($app)
+    {
+        return [
+            CategoryServiceProvider::class
+        ];
+    }
+
+    protected function getPackageAliases($app)
+    {
+        return [];
+    }
 }
