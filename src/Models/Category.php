51a80d1d16b880121fd5705edea55c7e2fb91355
jabber <2898117012@qq.com>
2022-06-01 05:51:25 +0800

    init

--- src/Models/Category.php
+++ src/Models/Category.php
@@ -6,11 +6,12 @@ use Dealskoo\Admin\Traits\HasSlug;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Laravel\Scout\Searchable;
 use Dealskoo\Country\Traits\HasCountry;
 
 class Category extends Model
 {
-    use HasFactory, SoftDeletes, HasCountry, HasSlug;
+    use HasFactory, SoftDeletes, HasCountry, HasSlug, Searchable;
 
     protected $fillable = [
         'slug',
@@ -28,5 +29,14 @@ class Category extends Model
     public function children()
     {
         return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    public function toSearchableArray()
+    {
+        return $this->only([
+            'slug',
+            'name',
+            'country_id'
+        ]);
     }
 }
